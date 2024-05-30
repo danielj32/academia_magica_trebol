c06b71c560a6c29d68f0104ec19d43421484763c
danielj32 <danieljuanito21@hotmail.com>
2024-05-29 23:32:03 -0500

    se añade plantilla index

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,6 +7,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from routes import *
+
+@app.route('/')
+def index():
+    solicitudes = Solicitud.query.all()
+    return render_template('index.html', solicitudes=solicitudes)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
